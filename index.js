a567815b328c1184816743aa1394c9eafb928216
CodeyWorley <codeyworley@gmail.com>
2019-01-25 09:10:52 -0700

    added get and delete routes

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+// Animal list
+let cats = [
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+];
+let dogs = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
+];
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -21,6 +45,25 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+
+// routes
+app.get('/api/cat', function(req, res) {
+  res.json(cats[0]);
+})
+
+app.delete('/api/cat', function(req, res) {
+  cats.shift()
+  res.send('cat adopted');
+})
+
+app.get('/api/dog', function(req, res) {
+  res.json(dogs[0]);
+})
+
+app.delete('/api/dog', function(req, res) {
+  dogs.shift()
+  res.send('dog adopted');
+})
 
 function runServer(port = PORT) {
   const server = app
